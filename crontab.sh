e1f646314b527ccadf8afd34e5666c7e0cfa82e7
joao uchoa <joao.victor@genomika.com.br>
2017-10-31 18:43:36 +0000

    Update crontab.sh

--- crontab.sh
+++ crontab.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-(crontab -l 2>/dev/null; echo "30 02 * * * python /var/www/backupy/run.py") | crontab -
-(crontab -l 2>/dev/null; echo "30 13 * * * python /var/www/backupy/run.py") | crontab -
+(crontab -l 2>/dev/null; echo "00 * * * * python /var/www/backupy/run.py") | crontab -
